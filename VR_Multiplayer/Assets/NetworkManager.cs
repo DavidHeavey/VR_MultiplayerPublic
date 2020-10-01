0dea6488260fc23ede38e963af35be790756eb24
DavidHeavey <64201109+DavidHeavey@users.noreply.github.com>
2020-10-01 18:55:30 +0100

    VR online classroom

---
+++ VR_Multiplayer/Assets/NetworkManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ConnectToServer();
+    }
+
+    private void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Connecting to Server...");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected Tos Server");
+        base.OnConnectedToMaster();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 10;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+        PhotonNetwork.JoinOrCreateRoom("AIT VR grouproom", roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log(" Joined the room");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("A new player joined the room");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+
+}
---
+++ VR_Multiplayer/Assets/XRGrabNetworkInteractable.cs
@@ -0,0 +1,26 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRGrabNetworkInteractable : XRGrabInteractable
+{
+    private PhotonView photonView;
+
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    protected override void OnSelectEnter(XRBaseInteractor interactor)
+    {
+        photonView.RequestOwnership();
+        base.OnSelectEnter(interactor);
+    }
+}
